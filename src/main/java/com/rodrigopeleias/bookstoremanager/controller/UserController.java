4b404bef0c9fbe6999fa2006a81c73bcf07d98dc
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-06-21 21:19:44 -0300

    Created feature developed

--- src/main/java/com/rodrigopeleias/bookstoremanager/controller/UserController.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/controller/UserController.java
@@ -4,10 +4,13 @@ import com.rodrigopeleias.bookstoremanager.controller.docs.UserControllerDoc;
 import com.rodrigopeleias.bookstoremanager.dto.MessageDTO;
 import com.rodrigopeleias.bookstoremanager.dto.UserDTO;
 import com.rodrigopeleias.bookstoremanager.exception.UserAlreadyExistsException;
+import com.rodrigopeleias.bookstoremanager.exception.UserNotExistsException;
 import com.rodrigopeleias.bookstoremanager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,5 +30,11 @@ public class UserController implements UserControllerDoc {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageDTO create(@RequestBody @Valid UserDTO userToSaveDTO) throws UserAlreadyExistsException {
         return userService.create(userToSaveDTO);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public MessageDTO update(@PathVariable Long id, @RequestBody @Valid UserDTO userToUpdateDTO) throws UserAlreadyExistsException, UserNotExistsException {
+        return userService.update(id, userToUpdateDTO);
     }
 }
--- src/main/java/com/rodrigopeleias/bookstoremanager/service/UserService.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/service/UserService.java
@@ -4,6 +4,7 @@ import com.rodrigopeleias.bookstoremanager.dto.MessageDTO;
 import com.rodrigopeleias.bookstoremanager.dto.UserDTO;
 import com.rodrigopeleias.bookstoremanager.entity.User;
 import com.rodrigopeleias.bookstoremanager.exception.UserAlreadyExistsException;
+import com.rodrigopeleias.bookstoremanager.exception.UserNotExistsException;
 import com.rodrigopeleias.bookstoremanager.mapper.UserMapper;
 import com.rodrigopeleias.bookstoremanager.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -27,8 +28,37 @@ public class UserService {
         return creationMessage(createdUser);
     }
 
+    public MessageDTO update(Long id, UserDTO userToUpdateDTO) throws UserNotExistsException {
+        User foundUser = verifyIfExists(id);
+
+        userToUpdateDTO.setId(id);
+        User userToUpdate = userMapper.toModel(userToUpdateDTO);
+        userToUpdate.setCreatedDate(foundUser.getCreatedDate());
+        userToUpdate.setCreatedBy(foundUser.getCreatedBy());
+
+        User updatedUser = userRepository.save(userToUpdate);
+        return updateMessage(updatedUser);
+    }
+
+
+    private User verifyIfExists(Long id) throws UserNotExistsException {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotExistsException(id));
+    }
+
+    private void verifyIfExists(String email, String username) throws UserAlreadyExistsException {
+        Optional<User> foundUser = userRepository.findByEmailOrUsername(email, username);
+        if (foundUser.isPresent()) {
+            throw new UserAlreadyExistsException(email, username);
+        }
+    }
+
     private MessageDTO creationMessage(User createdUser) {
         return returnMessage("created", createdUser);
+    }
+
+    private MessageDTO updateMessage(User updatedUser) {
+        return returnMessage("updated", updatedUser);
     }
 
     private MessageDTO returnMessage(String action, User user) {
@@ -37,11 +67,5 @@ public class UserService {
                 .build();
     }
 
-    private void verifyIfExists(String email, String username) throws UserAlreadyExistsException {
-        Optional<User> foundUser = userRepository.findByEmailOrUsername(email, username);
-        if (foundUser.isPresent()) {
-            throw new UserAlreadyExistsException(email, username);
-        }
 
-    }
 }
